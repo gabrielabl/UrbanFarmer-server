818c9c78d0374b9a845a24838eb4efcdb4930c90
Gabriela <lopes.gb18@gmail.com>
2023-08-12 17:28:45 -0400

    DB-migration&seed-users-email-added

--- seed_data/users.js
+++ seed_data/users.js
@@ -3,6 +3,7 @@ module.exports=[
     {
     id: "4aefea6d-7ecf-4739-9c47-84f437d78844",
     user_name: "Adan",
+    email:"adan@email.com",
     password: "password",
     avatar_photo: "avatar-user3.jpg",
     city: "Ziemannton",
@@ -15,6 +16,7 @@ module.exports=[
     {
     id: "76e3db8b-037d-4f69-9fa8-4b0885b0723a",
     user_name: "Dell",
+    email:"dell@email.com",
     password: "password",
     avatar_photo: "avatar-user2.jpg",
     city: "North Hobartberg",
@@ -27,6 +29,7 @@ module.exports=[
     {
     id: "79ccf5a2-2e83-413e-9261-974ef4af27b8",
     user_name: "Melyna",
+    email:"melyna@email.com",
     password: "password",
     avatar_photo: "avatar-user.jpg",
     city: "Ortizborough",
@@ -39,6 +42,7 @@ module.exports=[
     {
     id: "ca09943a-3cec-48c3-9052-264b1dc149ab",
     user_name: "Gabriela",
+    email:"gabriela@email.com",
     password: "password",
     avatar_photo: "gabriela_user.jpg",
     city: "Toronto",

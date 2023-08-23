6d188ea852aae996e0f58a9d7f38e0b970cce47c
Gabriela <lopes.gb18@gmail.com>
2023-08-22 20:32:34 -0400

    req.body-login-renam

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -57,14 +57,14 @@ exports.signUp = (req, res) => {
 };
 
 exports.login = (req, res) => {
-  const { email, password } = req.body;
+  const { emailLogin, passwordLogin } = req.body;
   knex("users")
-    .where({ email: email })
+    .where({ email: emailLogin })
     .then((data) => {
       const user = data[0];
       if (!user) {
         res.status(403).json({ error: "User not found in database" });
-      } else if (bcrypt.compareSync(password, user.password)) {
+      } else if (bcrypt.compareSync(passwordLogin, user.password)) {
         const token = jwt.sign({ email: user.email, userId: user.id}, SECRET_KEY);
         res.status(200).json({
           token: token,

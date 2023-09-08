1d09482cb126b6e71c5bc6e3dccd228df55c0dfd
Gabriela <lopes.gb18@gmail.com>
2023-09-07 20:40:31 -0400

    env.variable-local-host

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 module.exports = {
   client: 'mysql',
   connection: {
-    host: '127.0.0.1',
+    host: process.env.DB_LOCAL_HOST,
     database: process.env.DB_LOCAL_DBNAME,
     user: process.env.DB_LOCAL_USER,
     password: process.env.DB_LOCAL_PASSWORD,

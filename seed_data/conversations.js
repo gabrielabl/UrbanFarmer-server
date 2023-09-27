369db78f2ae7402a6809c264f1053f4441173470
Gabriela <lopes.gb18@gmail.com>
2023-09-27 19:49:10 -0400

    seed_data

---
+++ seed_data/conversations.js
@@ -0,0 +1,24 @@
+
+//CONVERSATIONS
+module.exports = [
+  {
+    id: "60c0e5df-2312-440e-9e12-e56ffc302191",
+    conversation_name: "Liliane",
+  },
+  {
+    id: "7d4ed0da-5682-4588-8586-16eb5bd50dd4",
+    conversation_name: "Curt",
+  },
+  {
+    id: "91c0982d-7186-4f51-903c-2d79ff231431",
+    conversation_name: "Royce",
+  },
+  {
+    id: "b927d484-9762-4234-ad19-0adce8d25b0b",
+    conversation_name: "Elwyn",
+  },
+  {
+    id: "f397dd7b-cdd6-4f30-9d9c-044efe59e432",
+    conversation_name: "Anna",
+  },
+];
